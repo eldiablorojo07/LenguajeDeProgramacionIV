24c98b76411f203816f52bcab0658700d59cf54d
unknown <munozmicoloingo913@.com>
2025-06-04 20:36:11 -0600

    repocitorio agregue

---
+++ scanner/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("ingresa un valor patra X");
+        String x=sc.();
+        System.out.print("ingresa un valor patra Y");
+        double y= sc.nextDouble();
+        double res=x+y;
+        System.out.print(res);
+    }
+}
